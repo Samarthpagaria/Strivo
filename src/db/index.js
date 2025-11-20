492add15276a655eb587e14412fac28568b3b96c
pagariasamarth@gmail.com <pagariasamarth@gmail.com>
2025-11-20 12:35:25 +0530

    add user controller and bug fixes

--- src/db/index.js
+++ src/db/index.js
@@ -21,3 +21,4 @@ export default connectDB;
 // Homework assignments / Tasks :
 // --Get to know about process in nodejs
 // -- Get to know about connectionInstance(variable ) actually console.log the variable which stores the return obeject from the mongoose.connect() function.
+
