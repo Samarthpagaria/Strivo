bf46368cf7e5b70690c309226a05bc480dc7fb1b
pagariasamarth@gmail.com <pagariasamarth@gmail.com>
2025-11-29 01:01:00 +0530

    add routes for the dashboard

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ import tweetRouter from "./routes/tweets.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
-
+import dashboardRouter from "./routes/dashboard.routes.js";
 //routes declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/healthcheck", healthcheckRouter);
@@ -34,5 +34,5 @@ app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/playlist", playlistRouter);
-
+app.use("api/v1/dashboard", dashboardRouter);
 export { app };
---
+++ src/routes/dashboard.routes.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware";
+const router = Router();
+router.use(verifyJWT);
+router.route("/stats").get(getChannelStats);
+router.route("/videos").get(getChannelVideos);
