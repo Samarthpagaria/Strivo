80c3e8a390375eb710d70d9754bbc166cd42ba2f
pagariasamarth@gmail.com <pagariasamarth@gmail.com>
2025-12-02 15:32:05 +0530

    Initialize routing for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,11 @@
 // App.jsx (or App.tsx)
-
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
-// 1. Correct the import to Framer Motion
-import { motion } from "framer-motion";
-import { Button } from "@/components/ui/button";
-
-// 2. Create the Motion Button component
-const MotionButton = motion(Button);
 
 function App() {
   return (
     <>
-      <div className="flex justify-center items-center h-screen">
-        <motion.h1
-          animate={{ rotate: 360 }}
-          transition={{ duration: 2 }}
-          className="bg-amber-300 rounded-xl p-2 text-2xl font-bold text-zinc-900 mx-10 w-fit"
-        >
-          Hello from Strivo
-        </motion.h1>
-
-        {/* 3. Use the new wrapped component */}
-        <MotionButton
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-          transition={{ type: "spring", stiffness: 400, damping: 10 }}
-        >
-          Click me
-        </MotionButton>
-      </div>
+      <RouterProvider router={router} />
     </>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+/*
+Initializing router in the project 
+1.Add RouterProvider in app.jsx
+2.create route folder and router.jsx file
+3.import createBrowserRouter in app.jsx
+4.create router in router.jsx file
+5.use router in app.jsx
+*/
