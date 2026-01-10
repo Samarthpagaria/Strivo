2818a6886b2ee7a66a7d0b1d42495e16f0cd0d3f
pagariasamarth@gmail.com <pagariasamarth@gmail.com>
2026-01-10 20:33:07 +0530

    feat: Create endpoint of home-feed-tweets

--- backend/src/controllers/tweets.controllers.js
+++ backend/src/controllers/tweets.controllers.js
@@ -2,13 +2,12 @@ import { isValidObjectId } from "mongoose";
 import mongoose from "mongoose";
 import { User } from "../models/user.models.js";
 import { Tweet } from "../models/tweet.models.js";
+import { Subscription } from "../models/subscription.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-  //TODO: create tweet
-  //get userid from req.user -> req.body (content) ->save t database by ceate->
   const userId = req.user?._id;
   if (!userId || !isValidObjectId(userId)) {
     throw new ApiError(400, "Invalid userId");
@@ -27,7 +26,6 @@ const createTweet = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, tweet, "Tweet Created Successfully"));
 });
 const getUserTweets = asyncHandler(async (req, res) => {
-  //TODO: get user tweets
   const userId = req.params.userId;
   if (!userId || !isValidObjectId(userId)) {
     throw new ApiError(400, "Invalid userId");
@@ -93,7 +91,7 @@ const updateTweet = asyncHandler(async (req, res) => {
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
-  // TODO: delete tweet
+
   const tweetId = req.params.tweetId;
   if (!tweetId || !isValidObjectId(tweetId)) {
     throw new ApiError(400, "Invalid tweetId");
@@ -118,4 +116,140 @@ const deleteTweet = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, deletedTweet, "Tweet deleted Successfully"));
 });
 
-export { createTweet, getUserTweets, updateTweet, deleteTweet };
+const getHomeFeedTweets = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized access");
+  }
+
+  const subscriptions = await Subscription.find({ subscriber: userId }).select(
+    "channel"
+  );
+  const channelIds = subscriptions.map((s) => s.channel);
+
+  const subscribedTweets = await Tweet.aggregate([
+    {
+      $match: {
+        owner: { $in: channelIds },
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "tweet",
+        as: "likes",
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "ownerDetails",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    { $unwind: "$ownerDetails" },
+    {
+      $addFields: {
+        likesCount: { $size: "$likes" },
+        isLiked: {
+          $cond: {
+            if: {
+              $in: [new mongoose.Types.ObjectId(userId), "$likes.likedBy"],
+            },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    { $project: { likes: 0 } },
+    { $sort: { createdAt: -1 } },
+    { $limit: 20 },
+  ]);
+
+  const randomTweets = await Tweet.aggregate([
+    { $sample: { size: 30 } },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "tweet",
+        as: "likes",
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "ownerDetails",
+        pipeline: [
+          {
+            $project: {
+              username: 1,
+              fullName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    { $unwind: "$ownerDetails" },
+    {
+      $addFields: {
+        likesCount: { $size: "$likes" },
+        isLiked: {
+          $cond: {
+            if: {
+              $in: [new mongoose.Types.ObjectId(userId), "$likes.likedBy"],
+            },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    { $project: { likes: 0 } },
+  ]);
+
+  const subscribedTweetIds = new Set(
+    subscribedTweets.map((t) => t._id.toString())
+  );
+  const filteredRandomTweets = randomTweets.filter(
+    (t) => !subscribedTweetIds.has(t._id.toString())
+  );
+
+  const combinedFeed = [...subscribedTweets, ...filteredRandomTweets];
+
+
+  combinedFeed.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        combinedFeed,
+        "Home feed tweets fetched successfully"
+      )
+    );
+});
+
+export {
+  createTweet,
+  getUserTweets,
+  updateTweet,
+  deleteTweet,
+  getHomeFeedTweets,
+};
--- backend/src/routes/tweets.routes.js
+++ backend/src/routes/tweets.routes.js
@@ -5,12 +5,14 @@ import {
   updateTweet,
   deleteTweet,
   getUserTweets,
+  getHomeFeedTweets,
 } from "../controllers/tweets.controllers.js";
 const router = Router();
 
 router.use(verifyJWT);
 
 router.route("/").post(createTweet);
+router.route("/feed").get(getHomeFeedTweets);
 router.route("/user/:userId").get(getUserTweets);
 router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
 export default router;
